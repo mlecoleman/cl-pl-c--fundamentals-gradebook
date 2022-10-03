6e32c2a391eb2101c1fd13ade82877c0d5423d7b
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2022-10-03 18:27:00 -0400

    Initial commit - plauralsight c# fundamentals project

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            double result = 0.0;
+            foreach(double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Jello!"); 
+            }
+        }
+    }
+}
