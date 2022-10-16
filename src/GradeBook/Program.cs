f46a7573da1da7b3b96c7340ff3a831cb7bb0fb3
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2022-10-16 18:56:26 -0400

    Add unit tests and refactor

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,25 +9,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
+            var book = new Book("Coleman's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
 
-            double result = 0.0;
-            foreach(double number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
+            var stats = book.GetStatistics();
 
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Jello!"); 
-            }
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}"); 
         }
     }
 }
